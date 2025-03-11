f4c5e4d5a411a0f617a1208ccc8a84a1278ed186
MuhammadWeb97 <allayevmuhammad9@gmail.com>
2025-03-11 14:12:20 +0500

    second

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -2,7 +2,151 @@ import React from 'react'
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='menu-section'>
+        <h2 class="menu-top-text">
+                O'z Burgeringizni Tanlang
+            </h2>
+            <div class="menu-list">
+                <div class="menu-item">
+                    <img src="/2.png" alt=""/>
+                    <h3 class="item-head">
+                        Bekon va tovuq bilan barbekyu
+                    </h3>
+                    <p class="item-info">
+                        Susan urug'lari, tovuq kotleti, kadr pishloqi, pomidor, tuzlangan bodring, tuzlangan piyoz, romain salatasi, bekon, barbekyu sousi bilan briochli bulochka
+                    </p>
+                    <div class="menu-item-bottom">
+                        <div class="prices">
+                            <h3 class="price">
+                                7$
+                            </h3>
+                            <p class="massa">
+                                390 gr
+                            </p>
+                        </div>
+                        <button class="order-btn">
+                            Buyurtma berish
+                            <img src="/Vector.png" alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div class="menu-item">
+                    <img src="/1.png" alt=""/>
+                    <h3 class="item-head">
+                        Bekon va tovuq bilan barbekyu
+                    </h3>
+                    <p class="item-info">
+                        Susan urug'lari, tovuq kotleti, kadr pishloqi, pomidor, tuzlangan bodring, tuzlangan piyoz, romain salatasi, bekon, barbekyu sousi bilan briochli bulochka
+                    </p>
+                    <div class="menu-item-bottom">
+                        <div class="prices">
+                            <h3 class="price">
+                                7$
+                            </h3>
+                            <p class="massa">
+                                390 gr
+                            </p>
+                        </div>
+                        <button class="order-btn">
+                            Buyurtma berish
+                            <img src="/Vector.png" alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div class="menu-item">
+                    <img src="/3.png" alt=""/>
+                    <h3 class="item-head">
+                        Bekon va tovuq bilan barbekyu
+                    </h3>
+                    <p class="item-info">
+                        Susan urug'lari, tovuq kotleti, kadr pishloqi, pomidor, tuzlangan bodring, tuzlangan piyoz, romain salatasi, bekon, barbekyu sousi bilan briochli bulochka
+                    </p>
+                    <div class="menu-item-bottom">
+                        <div class="prices">
+                            <h3 class="price">
+                                7$
+                            </h3>
+                            <p class="massa">
+                                390 gr
+                            </p>
+                        </div>
+                        <button class="order-btn">
+                            Buyurtma berish
+                            <img src="/Vector.png" alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div class="menu-item">
+                    <img src="/4.png" alt=""/>
+                    <h3 class="item-head">
+                        Bekon va tovuq bilan barbekyu
+                    </h3>
+                    <p class="item-info">
+                        Susan urug'lari, tovuq kotleti, kadr pishloqi, pomidor, tuzlangan bodring, tuzlangan piyoz, romain salatasi, bekon, barbekyu sousi bilan briochli bulochka
+                    </p>
+                    <div class="menu-item-bottom">
+                        <div class="prices">
+                            <h3 class="price">
+                                7$
+                            </h3>
+                            <p class="massa">
+                                390 gr
+                            </p>
+                        </div>
+                        <button class="order-btn">
+                            Buyurtma berish
+                            <img src="/Vector.png" alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div class="menu-item">
+                    <img src="/5.png" alt=""/>
+                    <h3 class="item-head">
+                        Bekon va tovuq bilan barbekyu
+                    </h3>
+                    <p class="item-info">
+                        Susan urug'lari, tovuq kotleti, kadr pishloqi, pomidor, tuzlangan bodring, tuzlangan piyoz, romain salatasi, bekon, barbekyu sousi bilan briochli bulochka
+                    </p>
+                    <div class="menu-item-bottom">
+                        <div class="prices">
+                            <h3 class="price">
+                                7$
+                            </h3>
+                            <p class="massa">
+                                390 gr
+                            </p>
+                        </div>
+                        <button class="order-btn">
+                            Buyurtma berish
+                            <img src="/Vector.png" alt=""/>
+                        </button>
+                    </div>
+                </div>
+                <div class="menu-item">
+                    <img src="/6.png" alt=""/>
+                    <h3 class="item-head">
+                        Bekon va tovuq bilan barbekyu
+                    </h3>
+                    <p class="item-info">
+                        Susan urug'lari, tovuq kotleti, kadr pishloqi, pomidor, tuzlangan bodring, tuzlangan piyoz, romain salatasi, bekon, barbekyu sousi bilan briochli bulochka
+                    </p>
+                    <div class="menu-item-bottom">
+                        <div class="prices">
+                            <h3 class="price">
+                                7$
+                            </h3>
+                            <p class="massa">
+                                390 gr
+                            </p>
+                        </div>
+                        <button class="order-btn">
+                            Buyurtma berish
+                            <img src="/Vector.png" alt=""/>
+                        </button>
+                    </div>
+                </div>
+            </div>
+    </div>
   )
 }
 
--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const Order = () => {
   return (
-    <div>Order</div>
+    <div className='order-section'>
+        <h2 className="order-top-text">
+                Buyurtma berish
+            </h2>
+            <div className="img-box">
+                <img src="./images/box.png" alt=""/>
+            </div>
+            <div className="order-cont">
+                <div style={{width:'250px'}}>
+                    <h3>
+                        Barcha ma'lumotlarni to'ldiring va menejerimiz buyurtmani tasdiqlash uchun siz bilan bog'lanadi    
+                    </h3>
+                </div>
+                <br/>
+                <div className="input-grad">
+                    <input type="text" className="order-input"/>
+                </div>
+                <div className="input-grad">
+                    <input type="text" className="order-input"/>
+                </div>
+                <div className="input-grad">
+                    <input type="text" className="order-input"/>
+                </div>
+                <button className="orderBtn">
+                    Buyutma berish
+                </button>
+            </div>
+    </div>
   )
 }
 
